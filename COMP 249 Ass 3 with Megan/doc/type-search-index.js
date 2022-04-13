5ada3475a0cb9d14e076c7077f1636bb11518655
Inas Fawzi <95307617+inas-fawzi@users.noreply.github.com>
2022-04-13 12:04:06 -0400

    Delete COMP 249 Ass 3 with Megan directory

--- COMP 249 Ass 3 with Megan/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CSVAttributeMissing"},{"p":"<Unnamed>","l":"CSVDataMissing"},{"p":"<Unnamed>","l":"Driver"}];updateSearchResults();
